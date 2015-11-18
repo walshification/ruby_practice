2181b19ec0577cae090d49285dd983fc9abbaf6b
Christopher Walsh <walshification@gmail.com>
2015-11-17 21:05:12 -0600

    passes common_element.rb

--- common_element.rb
+++ common_element.rb
@@ -1,5 +1,10 @@
 # Find the common element between two arrays.
-
+def common_element(a_1, a_2)
+  a_1.each do |el|
+    return el if a_2.include?(el)
+  end
+  nil
+end
 
 # Driver code
 p common_element([2, 3, 2], [1, 9, 3]) # should be 3
