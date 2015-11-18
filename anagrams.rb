5223a916c14c2f0df1030a50dd112a180ea25f04
Christopher Walsh <walshification@gmail.com>
2015-11-17 20:23:35 -0600

    passes anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -1,5 +1,17 @@
 # Determine if two strings are anagrams (case insensitive)
+def anagrams?(s_1, s_2)
+  return "Must use two strings" unless s_1.is_a?(String) && s_2.is_a?(String)
+  char_counter(s_1) == char_counter(s_2)
+end
 
+# Return a hash of characters and their count in a string
+def char_counter(string)
+  char_count = Hash.new
+  string.downcase.split(//).each do |char|
+    char_count[char] = (char_count[char] || 0) + 1
+  end
+  char_count
+end
 
 # Driver code
 p anagrams?("cinema", "iceman") # should be true
