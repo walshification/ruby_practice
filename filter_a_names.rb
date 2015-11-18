16649871bda3a3e3da1bba5f8c1ea699db2273b7
Christopher Walsh <walshification@gmail.com>
2015-11-17 21:09:40 -0600

    passes filter_a_names

--- filter_a_names.rb
+++ filter_a_names.rb
@@ -1,5 +1,9 @@
 # Filter an array of names to only contain names that start with the letter "A".
-
+def filter_a_names(name_array)
+  a_names = Array.new
+  name_array.map { |name| a_names << name if name[0].downcase == 'a' }
+  a_names
+end
 
 # Driver code
 p filter_a_names(["Bob", "Charlie", "Angel"]) # should be ["Angel"]
