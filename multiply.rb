532017da3791780fa26353153c84599e5de9f3eb
Christopher Walsh <walshification@gmail.com>
2015-11-18 00:15:37 -0600

    passes multiply

--- multiply.rb
+++ multiply.rb
@@ -1,5 +1,13 @@
 # Write a method that multiplies two positive integers without using *
-
+def multiply(n1, n2)
+  return 0 if n1 == 0 || n2 == 0
+  product = 0
+  while n1 > 0
+    product += n2
+    n1 -= 1
+  end
+  product
+end
 
 # Driver code
 p multiply(3, 4) # should be 12
