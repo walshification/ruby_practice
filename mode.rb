0721d51b22e62d80933417e45ae5a36741cc87d7
Christopher Walsh <walshification@gmail.com>
2015-11-18 00:04:59 -0600

    passes mode

--- mode.rb
+++ mode.rb
@@ -1,5 +1,12 @@
 # HARD: Find the mode (the most frequent integer) in an array of integers
-
+def mode(num_list)
+  mode_nums, num_count = Array.new, Hash.new
+  num_list.each { |n| num_count[n] = (num_count[n] || 0) + 1 }
+  num_count.each do |n, count|
+    mode_nums << n if count == num_count.values.max
+  end
+  return mode_nums.first if mode_nums.length == 1 else mode_nums
+end
 
 # Driver code
 p mode([2, 4, 5, 2]) # should be 2
